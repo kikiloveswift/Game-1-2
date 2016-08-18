a7fc445f39005eb2ef7468309cc4e63ab8bf3f75
konglee <konglee873@gmail.com>
2016-08-18 17:37:46 +0800

    Initial Commit

---
+++ Game-1-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game-1-2
+//
+//  Created by kong on 16/8/18.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
